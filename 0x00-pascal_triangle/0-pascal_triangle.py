af3d83c087fdde7f3df3a6ffe5ce984c3a717f63
mina <minaraafatemil14@gmail.com>
2024-08-08 22:44:57 +0300

    MO

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    # Initialize the triangle with the first row
+    my_arr = [[1]]
+
+    for i in range(1, n):  # Start from the second row
+        row = [1]  # First element of each row is always 1
+        for j in range(1, i):  # Generate the intermediate elements
+            row.append(my_arr[i-1][j-1] + my_arr[i-1][j])
+        row.append(1)  # Last element of each row is always 1
+        my_arr.append(row)
+
+    return my_arr
+
+
+# print(pascal_triangle(5))
+# my_arr = [[]]
+# my_arr[0] = [1]+[0]*(5-1)
+# print(my_arr)
+# print(my_arr[0][1])
