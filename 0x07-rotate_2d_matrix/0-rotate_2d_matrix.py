e125d17bed148539b0e28c7c98688e552f53f097
mina <minaraafatemil14@gmail.com>
2024-09-12 13:28:28 +0300

    MON

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,15 +18,3 @@ def rotate_2d_matrix(matrix):
             matrix[last][last - offset] = matrix[i][last]
             matrix[i][last] = top
             offset += 1
-
-# def rotate_2d_matrix(matrix):
-#     n = len(matrix)
-
-#     # Transpose the matrix (swap rows with columns)
-#     for i in range(n):
-#         for j in range(i, n):
-#             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-#     # Reverse the rows to complete the rotation
-#     for i in range(n):
-#         matrix[i].reverse()
